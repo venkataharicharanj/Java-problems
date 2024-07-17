24282b3bed5538ccf84ecf7953f86322eef67a12
venkataharicharanj <122023049+venkataharicharanj@users.noreply.github.com>
2024-07-17 20:34:53 +0530

    greater than 35 in array
    a program to count the elements in an array which are greater than 35.

---
+++ Main (2).java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc =new Scanner(System.in);
+	    int n= sc.nextInt();
+	     int arr[]=new int [n];
+	     for(int i=0;i<n;i++){
+	         arr[i]=sc.nextInt();
+	     }
+	     int count=0;
+	     for(int i=0;i<n;i++){
+	         if(arr[i]>35){
+	             count++;
+	         }
+	     }
+	         System.out.println(count);
+	}
+}
